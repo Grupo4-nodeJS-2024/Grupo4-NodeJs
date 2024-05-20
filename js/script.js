0f0d40fa5df9439631dc40237aea5ae94c7df97d
ngorellana <91963355+ngorellana@users.noreply.github.com>
2024-05-20 17:12:08 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -28,7 +28,7 @@ window.addEventListener('load', displayProducts);
 
 
 //FUNCION PARA LINKS DEL MENU PRINCIPAL
-document.addEventListener('DOMContentLoaded', () => {
+ document.addEventListener('DOMContentLoaded', () => {
     const contentDiv = document.getElementById('content');
     const links = document.querySelectorAll('.navbar-link');
 
@@ -37,15 +37,40 @@ document.addEventListener('DOMContentLoaded', () => {
         women: '<h2>Mujeres</h2><p>Ropa para mujeres.</p>',
         men: '<h2>Hombres</h2><p>Ropa para hombres.</p>',
         kids: '<h2>Juveniles</h2><p>Ropa para chicos</p>',
-        sustainable: '<h2>Sustentable</h2><p>Ropa sustentable</p>',
-        faq: '<h2>FAQ</h2><p>Preguntas Frecuentes</p>'
+        sustainable: '<h2>Sustentable</h2><p>Ropa sustentable</p>'
     };
+
+    function navigate(event) {
+        event.preventDefault();
+        const page = event.target.dataset.page;
+        contentDiv.innerHTML = contents[page];
+        updateActiveLink(event.target);
+        history.pushState({page}, '', `#${page}`);
+    }
+
     function updateActiveLink(activeLink) {
         links.forEach(link => {
             link.classList.remove('active');
         });
         activeLink.classList.add('active');
     }
+
+    links.forEach(link => {
+        link.addEventListener('click', navigate);
+    });
+
+    window.addEventListener('popstate', (event) => {
+        const page = event.state ? event.state.page : 'home';
+        contentDiv.innerHTML = contents[page];
+        const activeLink = Array.from(links).find(link => link.dataset.page === page);
+        updateActiveLink(activeLink);
+    });
+
+    const initialPage = location.hash.substring(1) || 'home';
+    contentDiv.innerHTML = contents[initialPage];
+    const initialLink = Array.from(links).find(link => link.dataset.page === initialPage);
+    updateActiveLink(initialLink);
+});
 
 
   // FORMULARIO
