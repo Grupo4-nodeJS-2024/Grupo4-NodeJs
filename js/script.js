ba968b447e5d4cf9ec66828adc6f9888da0d6fa0
ngorellana <91963355+ngorellana@users.noreply.github.com>
2024-05-22 18:27:41 -0500

    script.js
    Agregando Banner con js (eliminando el de Bootstrap para ajustar a requerimientos de TP)

--- js/script.js
+++ js/script.js
@@ -51,3 +51,78 @@ window.addEventListener('load', displayProducts);
     xhr.send(JSON.stringify(jsonData));
   });
   
+
+
+  //Función para linkear dentro de la página
+  
+  document.addEventListener('DOMContentLoaded', () => {
+    const contentDiv = document.getElementById('content');
+    const links = document.querySelectorAll('.navbar-link');
+
+    const contents = {
+        home: '<h2>Inicio</h2><p>primera página</p>',
+        women: '<h2>Mujeres</h2><p>Ropa para mujeres.</p>',
+        men: '<h2>Hombres</h2><p>Ropa para hombres.</p>',
+        kids: '<h2>Juveniles</h2><p>Ropa para chicos</p>',
+        sustainable: '<h2>Sustentable</h2><p>Ropa sustentable</p>'
+    };
+
+    function navigate(event) {
+        event.preventDefault();
+        const page = event.target.dataset.page;
+        contentDiv.innerHTML = contents[page];
+        updateActiveLink(event.target);
+        history.pushState({page}, '', `#${page}`);
+    }
+
+    function updateActiveLink(activeLink) {
+        links.forEach(link => {
+            link.classList.remove('active');
+        });
+        activeLink.classList.add('active');
+    }
+
+    links.forEach(link => {
+        link.addEventListener('click', navigate);
+    });
+
+    window.addEventListener('popstate', (event) => {
+        const page = event.state ? event.state.page : 'home';
+        contentDiv.innerHTML = contents[page];
+        const activeLink = Array.from(links).find(link => link.dataset.page === page);
+        updateActiveLink(activeLink);
+    });
+
+    const initialPage = location.hash.substring(1) || 'home';
+    contentDiv.innerHTML = contents[initialPage];
+    const initialLink = Array.from(links).find(link => link.dataset.page === initialPage);
+    updateActiveLink(initialLink);
+});
+  
+//BANNER//
+
+document.addEventListener("DOMContentLoaded", () => {
+  const bannerimgs = document.querySelectorAll('.bannerimg');
+  const prevButton = document.querySelector('.prev');
+  const nextButton = document.querySelector('.next');
+  let currentIndex = 0;
+
+  function updateBanner(index) {
+      bannerimgs.forEach((bannerimg, i) => {
+          bannerimg.classList.toggle('active', i === index);
+      });
+  }
+
+  prevButton.addEventListener('click', () => {
+      currentIndex = (currentIndex - 1 + bannerimgs.length) % bannerimgs.length;
+      updateBanner(currentIndex);
+  });
+
+  nextButton.addEventListener('click', () => {
+      currentIndex = (currentIndex + 1) % bannerimgs.length;
+      updateBanner(currentIndex);
+  });
+
+  // Initialize banner
+  updateBanner(currentIndex);
+});
