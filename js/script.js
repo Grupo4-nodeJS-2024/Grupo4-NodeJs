6aecea643e917e60db038521500989a7614a2687
MVC3520 <81169874+MVC3520@users.noreply.github.com>
2024-05-29 17:35:50 -0300

    cambios_29_05_24

--- js/script.js
+++ js/script.js
@@ -77,51 +77,7 @@ window.addEventListener('load', displayProducts);
 });
 
 
-  //Función para linkear dentro de la página
   
-  document.addEventListener('DOMContentLoaded', () => {
-    const contentDiv = document.getElementById('content');
-    const links = document.querySelectorAll('.navbar-link');
-
-    const contents = {
-        home: '<h2>Inicio</h2><p>primera página</p>',
-        women: '<h2>Mujeres</h2><p>Ropa para mujeres.</p>',
-        men: '<h2>Hombres</h2><p>Ropa para hombres.</p>',
-        kids: '<h2>Juveniles</h2><p>Ropa para chicos</p>',
-        sustainable: '<h2>Sustentable</h2><p>Ropa sustentable</p>'
-    };
-
-    function navigate(event) {
-        event.preventDefault();
-        const page = event.target.dataset.page;
-        contentDiv.innerHTML = contents[page];
-        updateActiveLink(event.target);
-        history.pushState({page}, '', `#${page}`);
-    }
-
-    function updateActiveLink(activeLink) {
-        links.forEach(link => {
-            link.classList.remove('active');
-        });
-        activeLink.classList.add('active');
-    }
-
-    links.forEach(link => {
-        link.addEventListener('click', navigate);
-    });
-
-    window.addEventListener('popstate', (event) => {
-        const page = event.state ? event.state.page : 'home';
-        contentDiv.innerHTML = contents[page];
-        const activeLink = Array.from(links).find(link => link.dataset.page === page);
-        updateActiveLink(activeLink);
-    });
-
-    const initialPage = location.hash.substring(1) || 'home';
-    contentDiv.innerHTML = contents[initialPage];
-    const initialLink = Array.from(links).find(link => link.dataset.page === initialPage);
-    updateActiveLink(initialLink);
-});
   
 //-------BANNER--------//
 
